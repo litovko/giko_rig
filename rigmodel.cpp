5f86f67abc32e1471208c2cc0960a3a86672dd84
litovko <sergei.litovko@gmail.com>
2015-11-07 13:51:53 +0300

    Настроена инициализация сервера, подключение клиента. Пока коннектится сам к себе.

--- rigmodel.cpp
+++ rigmodel.cpp
@@ -4,10 +4,17 @@
 
 cRigmodel::cRigmodel(QObject *parent) : QObject(parent)
 {
-    connect(this, SIGNAL(addressChanged()), this, SLOT(start_client())); // установили новый адрес - вызываем функцию start()
-    connect(&tcpServer, SIGNAL(newConnection()),this, SLOT(acceptConnection())); // новый коннект к серверу
-}
+    connect(this, SIGNAL(addressChanged()), this, SLOT(start_client())); // установили новый адрес - вызываем функцию start_clietn()
+    connect(this, SIGNAL(portChanged()), this, SLOT(start_client())); // установили новый порт - вызываем функцию start_clietn()
+    connect(&tcpServer, SIGNAL(newConnection()),this, SLOT(acceptConnection())); // получен новый коннект к серверу (пришел клиент)
+    connect(&tcpClient, SIGNAL(connected()),this, SLOT(clientConnected())); // Клиент приконнектилася к указанному адресу по указанному порту.
+    connect(&tcpClient, SIGNAL(bytesWritten(qint64)),this, SLOT(updateClientProgress(qint64)));
+    connect(&tcpClient, SIGNAL(error(QAbstractSocket::SocketError)), this, SLOT(displayError(QAbstractSocket::SocketError)));
 
+    tcpServer.listen(QHostAddress(m_address),m_port);
+    m_server_ready = true;
+}
+    
 void cRigmodel::setPressure(const int &pressure)
 {
     m_pressure = pressure;
@@ -42,6 +49,16 @@ QString cRigmodel::address() const
     return m_address;
 }
 
+int cRigmodel::port() const
+{
+    return m_port;
+}
+
+void cRigmodel::setPort(const int  &port)
+{
+    m_port = port;
+    emit portChanged();
+}
 bool cRigmodel::server_ready() const
 {
     return m_server_ready;
@@ -76,20 +93,65 @@ void cRigmodel::start_server()
 void cRigmodel::start_client()
 {
     bytesWritten = 0;
-    qDebug()<<"Start client >>>"+this->address();
-    tcpClient.connectToHost(address(), tcpServer.serverPort());
+    qDebug()<<"Start client >>>"+this->address()+" >> "+m_address+" >> "+m_port;
+    
+    tcpClient.connectToHost(m_address, m_port);
 }
+void cRigmodel::clientConnected()
+{
+    qDebug()<<"Client connect to address >>>"+this->address()+" port:"+ this->port();
+    m_client_connected=true;
+}
+void cRigmodel::clientDisconnected()
+{
+    qDebug()<<"Client disconnected form address >>>"+this->address()+" port:"+ this->port();
+    m_client_connected=false;
+}
+
 //Функция работает по сигналу прихода нового клиента к серверу.
 void cRigmodel::acceptConnection()
 {
     tcpServerConnection = tcpServer.nextPendingConnection();
-//    connect(tcpServerConnection, SIGNAL(readyRead()),
-//            this, SLOT(updateServerProgress()));
-//    connect(tcpServerConnection, SIGNAL(error(QAbstractSocket::SocketError)),
-//            this, SLOT(displayError(QAbstractSocket::SocketError)));
 
+    connect(tcpServerConnection, SIGNAL(readyRead()),
+            this, SLOT(updateServerProgress()));
+    connect(tcpServerConnection, SIGNAL(error(QAbstractSocket::SocketError)),
+            this, SLOT(displayError(QAbstractSocket::SocketError)));
+
+    qDebug()<<"Accepted connection >>>"+tcpServerConnection->peerAddress().toString();
     //serverStatusLabel->setText(tr("Accepted connection"));
     m_server_connected = true;
     tcpServer.close(); //выключаем листенер - одной коннеции достаточно.
     m_server_ready=false;
+}
+
+
+
+void cRigmodel::updateClientProgress(qint64 numBytes)
+{
+    // callen when the TCP client has written some bytes
+    bytesWritten += (int)numBytes;
+    qDebug()<<"Update client progress >>>"+numBytes;
+    // only write more if not finished and when the Qt write buffer is below a certain size.
+    if (bytesToWrite > 0 && tcpClient.bytesToWrite() <= 100)
+        bytesToWrite -= (int)tcpClient.write(QByteArray(qMin(bytesToWrite, 100), '@'));
+
+}
+void cRigmodel::updateServerProgress()
+{
+    bytesReceived += (int)tcpServerConnection->bytesAvailable();
+    tcpServerConnection->readAll();
+     qDebug()<<"Server progress bytes received >>>"+bytesReceived;
+
+
+}
+void cRigmodel::displayError(QAbstractSocket::SocketError socketError)
+{
+    if (socketError == QTcpSocket::RemoteHostClosedError)
+        return;
+    qDebug()<<"Network error >>>"+tcpClient.errorString();
+
+
+    tcpClient.close();
+    tcpServer.close();
 }
--- rigmodel.h
+++ rigmodel.h
@@ -21,22 +21,28 @@ class cRigmodel : public QObject
     Q_PROPERTY(bool server_connected READ server_connected NOTIFY server_connectedChanged)
 public:
     explicit cRigmodel(QObject *parent = 0);
+
     void setPressure(const int &pressure);
     int pressure() const;
+
     void setOiltemp(const int &oiltemp);
     int oiltemp() const;
+
     void setAddress(const QString  &address);
-    QString address();
+    QString address() const;
+
     void setPort(const int &port);
     int  port() const;
+
     bool server_ready() const;
     bool client_connected()const;
     bool server_connected()const;
-    QString address() const;
+
 signals:
     void pressureChanged();
     void oiltempChanged();
     void addressChanged();
+    void portChanged();
     void server_readyChanged();
     void client_connectedChanged();
     void server_connectedChanged();
@@ -44,14 +50,18 @@ signals:
 
 public slots:
     void start_server();
+    void start_client();
     void acceptConnection();  //tcpServer new connection
+    void clientConnected();
+    void clientDisconnected();
     void updateServerProgress();
     void updateClientProgress(qint64 numBytes);
+    void displayError(QAbstractSocket::SocketError socketError);
 private:
     int m_pressure=0;
     int m_oiltemp=0;
     QString m_address;
-    int m_port;
+    int m_port=65000;
     bool m_server_ready = false;
     bool m_client_connected = false;
     bool m_server_connected = false;
